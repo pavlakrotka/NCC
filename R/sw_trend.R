ed89bd23e0143bc770e51d3464271238ec8ebcde
Pavla Krotka <pavla.krotka@gmail.com>
2022-02-17 16:19:23 +0100

    update stepwise trend

--- R/sw_trend.R
+++ R/sw_trend.R
@@ -1,13 +1,12 @@
 #' Generation of stepwise trend with equal jumps between periods
 #'
-#' @description
+#' @description Generates stepwise trend for a given period. No time trend is assumed in the first period.
 #'
-#' @param cj a
-#' @param lambda a
+#' @param cj Period indicator
+#' @param lambda Strength of time trend
 #'
 #' @export
-#' @return
-#' @author
-#' @references
+#' @return Time trend in period cj
+#' @author Marta Bofill Roig, Pavla Krotka
 
 sw_trend <- function(cj,lambda){lambda*(cj-1)}
