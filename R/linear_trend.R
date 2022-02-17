0e7cdb395bd929d80d5146274ea3500b9395f3b2
Pavla Krotka <pavla.krotka@gmail.com>
2022-02-17 16:19:08 +0100

    update linear_trend

--- R/linear_trend.R
+++ R/linear_trend.R
@@ -1,14 +1,13 @@
-#' Generation of a linear trend
+#' Generation of a linear trend that starts in a given period
 #'
-#' @description
+#' @description Generates linear trend for patient j
 #'
-#' @param j a
-#' @param lambda a
-#' @param sample_size a
-#'
+#' @param j Patient recruitment index
+#' @param lambda Strength of time trend
+#' @param sample_size Vector of dimension 2, indicating sample size in the trial period until the time trend starts and the remaining sample size.
+#' 
 #' @export
-#' @return
-#' @author
-#' @references
+#' @return Time trend for patient j
+#' @author Marta Bofill Roig, Pavla Krotka
 
-linear_trend <- function(j, lambda, sample_size){lambda*(j-1)/(sample_size-1)}
+linear_trend <- function(j, lambda, sample_size){ifelse(j<=sample_size[1], 0, lambda*(j-1)/(sum(sample_size)-1))}
