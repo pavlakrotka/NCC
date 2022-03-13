e8924f5ba6ebe4753af731c1edce894c674068b3
Pavla Krotka <pavla.krotka@gmail.com>
2022-03-13 14:53:23 +0100

    hexsticker size

--- hexsticker.R
+++ hexsticker.R
@@ -4,10 +4,12 @@ library(showtext)
 font_add_google("Neucha", "Neucha")
 
 s <- sticker(magick::image_read(rsvg::rsvg_raw("man/figures/trial_hexsticker.svg")),
-             package="NCC", p_size=30, s_x=1, s_y=1.1, s_width=1.6, s_height=1, p_y=0.45,
-             h_fill = "#F5F5F5", h_color = "#8B0000", h_size = 1,
+             package="NCC", p_size=15, s_x=1, s_y=1.1, s_width=1.6, s_height=1, p_y=0.45,
+             h_fill = "#F5F5F5", h_color = "#8B0000", h_size = 0.5,
              p_color = "darkred", p_family = "Neucha",
              spotlight = F,
-             filename="man/figures/NCC_hexsticker.png")
+             filename = "man/figures/NCC_hexsticker.png")
 
 print(s)
+
+save_sticker("man/figures/NCC_hexsticker.png", sticker = s, scale = 0.5)
