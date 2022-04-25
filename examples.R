6482e4074119278bb98ac0e48663c5309a9af0c0
Pavla Krotka <pavla.krotka@gmail.com>
2022-04-25 18:31:29 +0200

    website update - Bayes, d whole trial

--- examples.R
+++ examples.R
@@ -7,16 +7,16 @@ set.seed(123)
 
 ## Continuous endpoints - equal linear time trends; alternative hypothesis
 
-cont_eq <- datasim_cont(n_total = 1000, num_arms = 3, d = 120, theta = rep(0.25, 3), lambda = rep(0.15, 4), sigma = 1, trend = "linear")
+cont_eq <- datasim_cont(num_arms = 3, n_arm = 100, d = c(0, 100, 250), theta = rep(0.25, 3), lambda = rep(0.15, 4), sigma = 1, trend = "linear")
 
 ## Continuous endpoints - different linear time trends; alternative hypothesis
 
-cont_diff <- datasim_cont(n_total = 1000, num_arms = 3, d = 120, theta = rep(0.25, 3), lambda = c(0.1, 0.15, 0.1, 0.1), sigma = 1, trend = "stepwise")
+cont_diff <- datasim_cont(num_arms = 3, n_arm = 100, d = c(0, 100, 250), theta = rep(0.25, 3), lambda = c(0.1, 0.15, 0.1, 0.1), sigma = 1, trend = "stepwise")
 
 ## Binary endpoints - equal linear time trends; alternative hypothesis
 
-bin_eq <- datasim_bin(n_total = 1000, num_arms = 3, d = 120, p0 = 0.7, OR = rep(1.8, 3), lambda = rep(0.15, 4), trend = "linear")
+bin_eq <- datasim_bin(num_arms = 3, n_arm = 100, d = c(0, 100, 250), p0 = 0.7, OR = rep(1.8, 3), lambda = rep(0.15, 4), trend = "linear")
 
-## Binary endpoints - different linear time trends; alternative
+## Binary endpoints - different linear time trends; alternative hypothesis
 
-bin_diff <- datasim_bin(n_total = 1000, num_arms = 3, d = 120, p0 = 0.7, OR = rep(1.8, 3), lambda = c(0.1, 0.15, 0.1, 0.1), trend = "linear")
+bin_diff <- datasim_bin(num_arms = 3, n_arm = 100, d = c(0, 100, 250), p0 = 0.7, OR = rep(1.8, 3), lambda = c(0.1, 0.15, 0.1, 0.1), trend = "linear")
