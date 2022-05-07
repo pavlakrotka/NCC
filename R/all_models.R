cfa4045b6f8be8c7bf34e8310426cf2f244dc7ce
Pavla Krotka <pavla.krotka@gmail.com>
2022-05-07 22:25:47 +0200

    flexible wrap up

--- R/all_models.R
+++ R/all_models.R
@@ -3,6 +3,9 @@
 #' @description Analyzes given data using the fixed effect model, pooled and separate analyses, and timemachine and MAP prior approach. Performs inference for all treatment arms in the trial except for the first one
 #'
 #' @param data Simulated trial data, e.g. result from the `datasim_bin()` or `datasim_cont()` function
+#' @param arms Vector with treatment arms to perform inference on. Default - all arms except the first one
+#' @param models Vector with models that should be used for the analysis. Default=c("fixmodel", "sepmodel", "poolmodel", "timemachine", "MAPprior")
+#' @param endpoint Endpoint indicator. "cont" for continuous endpoints, "bin" for binary endpoints
 #' @param alpha Type I error. Default=0.025
 #'
 #' @keywords internal
@@ -14,48 +17,27 @@
 #' trial_data <- datasim_bin(num_arms = 3, n_arm = 100, d = c(0, 100, 250),
 #' p0 = 0.7, OR = rep(1.8, 3), lambda = rep(0.15, 4), trend="stepwise")
 #' 
-#' all_models(data = trial_data)
+#' all_models(data = trial_data, arms = c(2,3), endpoint = "bin")
 #'
 #'
 #' @return List containing an indicator whether the null hypothesis was rejected or not for the investigated treatment for all models
 #' @author Pavla Krotka
 
 
-all_models <- function(data, alpha=0.025, ...){
+all_models <- function(data, arms, models = c("fixmodel", "sepmodel", "poolmodel", "timemachine", "MAPprior"), endpoint, alpha=0.025, ...){
+  
+  arms <- sort(arms)
+  models <- sort(models)
   
   res <- list()
   
-  if(all(data$response %in% c(0,1))){ # binary endpoints
-    
-    for (i in 2:max(data$treatment)) {
-      res_arm <- list(fixmodel_bin(data, arm = i, alpha)$reject_h0,
-                      sepmodel_bin(data, arm = i, alpha)$reject_h0,
-                      poolmodel_bin(data, arm = i, alpha)$reject_h0,
-                      timemachine_bin(data, arm = i, alpha, ...)$reject_h0,
-                      MAPprior_bin(data, arm = i, alpha, ...)$reject_h0)
+  for (i in arms) {
+    for (j in models) {
+      res_i_j <- list(do.call(paste0(j, "_", endpoint), list(data, arm = i, alpha))$reject_h0)
       
-      names(res_arm) <- c(paste0("reject_h0_fix_", i),
-                          paste0("reject_h0_sep_", i),
-                          paste0("reject_h0_pool_", i),
-                          paste0("reject_h0_timemachine_", i),
-                          paste0("reject_h0_MAPprior_", i))
+      names(res_i_j) <- paste0("reject_h0_", j, "_", i)
       
-      res <- append(res, res_arm)
-    }
-  } else { # continuous endpoints
-    
-    for (i in 2:max(data$treatment)) {
-      res_arm <- list(fixmodel_cont(data, arm = i, alpha)$reject_h0,
-                      sepmodel_cont(data, arm = i, alpha)$reject_h0,
-                      poolmodel_cont(data, arm = i, alpha)$reject_h0,
-                      timemachine_cont(data, arm = i, alpha, ...)$reject_h0)
-      
-      names(res_arm) <- c(paste0("reject_h0_fix_", i),
-                          paste0("reject_h0_sep_", i),
-                          paste0("reject_h0_pool_", i),
-                          paste0("reject_h0_timemachine_", i))
-      
-      res <- append(res, res_arm)
+      res <- append(res, res_i_j)
     }
   }
   return(res)
--- R/sim_study.R
+++ R/sim_study.R
@@ -4,6 +4,8 @@
 #'
 #' @param nsim Number of replications
 #' @param scenarios Data frame containing all parameters for scenarios that should be simulated
+#' @param arms Vector with treatment arms to perform inference on. Default - all arms except the first one
+#' @param models Vector with models that should be used for the analysis. Default=c("fixmodel", "sepmodel", "poolmodel", "timemachine", "MAPprior")
 #' @param endpoint Endpoint indicator. "cont" for continuous endpoints, "bin" for binary endpoints
 #'
 #' @keywords internal
@@ -15,15 +17,23 @@
 #' @author Pavla Krotka
 
 
-sim_study <- function(nsim, scenarios, endpoint){
+sim_study <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", "poolmodel", "timemachine", "MAPprior"), endpoint){
+  
+  models <- sort(models)
 
   if(endpoint=="cont"){
 
     result <- NULL
 
-    num_models <- 4
+    num_models <- length(models)
 
     for(i in 1:dim(scenarios)[1]){
+      
+      if(missing(arms)){
+        arms <- c(2:scenarios[i]$num_arms)
+      }
+      
+      arms <- sort(arms)
 
       d_i <- as.numeric(scenarios[i, grepl("^d\\d", names(scenarios))])
       theta_i <- as.numeric(scenarios[i, grepl("^theta\\d", names(scenarios))])
@@ -41,11 +51,14 @@ sim_study <- function(nsim, scenarios, endpoint){
                                                      trend = scenarios$trend[i],
                                                      N_peak = scenarios$N_peak[i],
                                                      full = FALSE),
+                                 arms = arms,
+                                 models = models,
+                                 endpoint = endpoint,
                                  alpha = scenarios$alpha[i]))
 
       result_i <- cbind(scenarios[i,],
-                        study_arm = rep(c(2:((nrow(db)/num_models)+1)), each = num_models), # inference for all arms starting with 2
-                        model = c("fix", "sep", "pool", "timemachine"),
+                        study_arm = rep(arms, each = num_models),
+                        model = models,
                         reject_h0 = rowMeans(matrix(unlist(unname(db)), ncol = nsim))) # get power/T1E
 
       result <- rbind(result, result_i)
@@ -57,9 +70,15 @@ sim_study <- function(nsim, scenarios, endpoint){
 
     result <- NULL
 
-    num_models <- 5
+    num_models <- length(models)
 
     for(i in 1:dim(scenarios)[1]){
+      
+      if(missing(arms)){
+        arms <- c(2:scenarios[i]$num_arms)
+      }
+      
+      arms <- sort(arms)
 
       d_i <- as.numeric(scenarios[i, grepl("^d\\d", names(scenarios))])
       OR_i <- as.numeric(scenarios[i, grepl("^OR\\d", names(scenarios))])
@@ -76,11 +95,14 @@ sim_study <- function(nsim, scenarios, endpoint){
                                                     trend = scenarios$trend[i],
                                                     N_peak = scenarios$N_peak[i],
                                                     full = FALSE),
+                                 arms = arms,
+                                 models = models,
+                                 endpoint = endpoint,
                                  alpha = scenarios$alpha[i]))
 
       result_i <- cbind(scenarios[i,],
-                        study_arm = rep(c(2:((nrow(db)/num_models)+1)), each = num_models), # inference for all arms starting with 2
-                        model = c("fix", "sep", "pool", "timemachine", "MAPprior"),
+                        study_arm = rep(arms, each = num_models),
+                        model = models,
                         reject_h0 = rowMeans(matrix(unlist(unname(db)), ncol = nsim))) # get power/T1E
 
       result <- rbind(result, result_i)
