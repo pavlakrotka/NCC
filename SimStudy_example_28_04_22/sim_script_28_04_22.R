33ff7fae8297b1902699d912115a1983c147c8ef
Pavla Krotka <pavla.krotka@gmail.com>
2022-05-12 11:18:38 +0200

    timemachine corrected results

--- SimStudy_example_28_04_22/sim_script_28_04_22.R
+++ SimStudy_example_28_04_22/sim_script_28_04_22.R
@@ -121,3 +121,108 @@ scenario_iii_alpha <- data.frame(num_arms = 4,
 results_iii_alpha <- sim_study(nsim = n_sim, scenarios = scenario_iii_alpha, endpoint = "bin")
 write_csv(results_iii_alpha, "results/results_iii_alpha.csv")
 
+
+#############################################################################################################################################################
+
+# Only timemachine (corrected code)
+
+scenario_ii_pow <- data.frame(num_arms = 3, 
+                              n_arm = 100, 
+                              d1 = 0,
+                              d2 = c(0, 100, 200),
+                              d3 = c(0, 200, 400),
+                              period_blocks = 2, 
+                              p0 = 0.3, 
+                              OR1 = 1.8,
+                              OR2 = 1.8,
+                              OR3 = 1.8,
+                              lambda0 = 0.3, 
+                              lambda1 = c(rep(0.3, 3), rep(0.5, 3)),
+                              lambda2 = 0.3,
+                              lambda3 = 0.3,
+                              trend = "stepwise",
+                              alpha = 0.1) %>%
+  mutate(timetrend = ifelse(lambda0==lambda1 & lambda1==lambda2 & lambda2==lambda3, "EQ", "DIFF"),
+         hypothesis = ifelse(OR2==1 & OR3==1, "H0", "H1"))
+
+results_ii_pow_TM <- sim_study(nsim = n_sim, scenarios = scenario_ii_pow, models = "timemachine", endpoint = "bin")
+write_csv(results_ii_pow_TM, "results/results_ii_pow_TM.csv")
+
+
+
+
+scenario_ii_alpha <- data.frame(num_arms = 3, 
+                                n_arm = 100, 
+                                d1 = 0,
+                                d2 = c(0, 100, 200),
+                                d3 = c(0, 200, 400),
+                                period_blocks = 2, 
+                                p0 = 0.3, 
+                                OR1 = 1.8,
+                                OR2 = 1,
+                                OR3 = 1,
+                                lambda0 = 0.3, 
+                                lambda1 = c(rep(0.3, 3), rep(0.5, 3)),
+                                lambda2 = 0.3,
+                                lambda3 = 0.3,
+                                trend = "stepwise",
+                                alpha = 0.1) %>%
+  mutate(timetrend = ifelse(lambda0==lambda1 & lambda1==lambda2 & lambda2==lambda3, "EQ", "DIFF"),
+         hypothesis = ifelse(OR2==1 & OR3==1, "H0", "H1"))
+
+results_ii_alpha_TM <- sim_study(nsim = n_sim, scenarios = scenario_ii_alpha, models = "timemachine", endpoint = "bin")
+write_csv(results_ii_alpha_TM, "results/results_ii_alpha_TM.csv")
+
+
+scenario_iii_pow <- data.frame(num_arms = 4, 
+                               n_arm = 100, 
+                               d1 = 0,
+                               d2 = 100,
+                               d3 = c(150, 200),
+                               d4 = 200,
+                               period_blocks = 2, 
+                               p0 = 0.3, 
+                               OR1 = 1.8,
+                               OR2 = 1.8,
+                               OR3 = 1.8,
+                               OR4 = 1.8,
+                               lambda0 = 0.3, 
+                               lambda1 = c(rep(0.3, 2), rep(0.5, 2)),
+                               lambda2 = 0.3,
+                               lambda3 = 0.3,
+                               lambda4 = 0.3,
+                               trend = "stepwise",
+                               alpha = 0.1) %>%
+  mutate(timetrend = ifelse(lambda0==lambda1 & lambda1==lambda2 & lambda2==lambda3 & lambda3==lambda4, "EQ", "DIFF"),
+         hypothesis = ifelse(OR2==1 & OR3==1 & OR4==1, "H0", "H1"))
+
+results_iii_pow_TM <- sim_study(nsim = n_sim, scenarios = scenario_iii_pow, models = "timemachine", endpoint = "bin")
+write_csv(results_iii_pow_TM, "results/results_iii_pow_TM.csv")
+
+
+
+scenario_iii_alpha <- data.frame(num_arms = 4, 
+                                 n_arm = 100, 
+                                 d1 = 0,
+                                 d2 = 100,
+                                 d3 = c(150, 200),
+                                 d4 = 200,
+                                 period_blocks = 2, 
+                                 p0 = 0.3, 
+                                 OR1 = 1.8,
+                                 OR2 = 1,
+                                 OR3 = 1,
+                                 OR4 = 1,
+                                 lambda0 = 0.3, 
+                                 lambda1 = c(rep(0.3, 2), rep(0.5, 2)),
+                                 lambda2 = 0.3,
+                                 lambda3 = 0.3,
+                                 lambda4 = 0.3,
+                                 trend = "stepwise",
+                                 alpha = 0.1) %>%
+  mutate(timetrend = ifelse(lambda0==lambda1 & lambda1==lambda2 & lambda2==lambda3 & lambda3==lambda4, "EQ", "DIFF"),
+         hypothesis = ifelse(OR2==1 & OR3==1 & OR4==1, "H0", "H1"))
+
+results_iii_alpha_TM <- sim_study(nsim = n_sim, scenarios = scenario_iii_alpha, models = "timemachine", endpoint = "bin")
+write_csv(results_iii_alpha_TM, "results/results_iii_alpha_TM.csv")
+
