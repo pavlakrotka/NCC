f1c032de806d7365c16c81edcf9ef68bfe78b64d
Pavla Krotka <pavla.krotka@gmail.com>
2022-06-02 13:55:30 +0200

    plot_trial

--- R/plot_trial.R
+++ R/plot_trial.R
@@ -7,25 +7,25 @@
 #' @import ggplot2
 #'
 #' @export
-#' 
-#' @examples 
-#' 
+#'
+#' @examples
+#'
 #' trial_data <- datasim_bin(num_arms = 3, n_arm = 100, d = c(0, 100, 250),
 #' p0 = 0.7, OR = rep(1.8, 3), lambda = rep(0.15, 4), trend="stepwise")
-#' 
-#' plottrial(treatments = trial_data$treatment)
+#'
+#' plot_trial(treatments = trial_data$treatment)
 #'
 #'
 #' @return ggplot showing trial progress over time
 #' @author Pavla Krotka
 
 
-plottrial <- function(treatments){
-  
+plot_trial <- function(treatments){
+
   ggplot() +
     geom_line(aes(x=c(1:length(treatments)), y=treatments, color=as.factor(treatments)), size=5, alpha=0.8) +
     labs(x="Time", y="Treatment") +
     theme_bw() +
     theme(legend.position="none")
-  
+
 }
