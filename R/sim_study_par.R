ca3706134269d6f709aa7a9cdc5182963e136f90
Pavla Krotka <pavla.krotka@gmail.com>
2022-07-11 15:28:11 +0200

    sim study parallel

--- R/sim_study_par.R
+++ R/sim_study_par.R
@@ -1,4 +1,4 @@
-#' Wrapper function performing simulation studies for a given set of scenarios
+#' Wrapper function performing simulation studies for a given set of scenarios (parallelized)
 #'
 #' @description Performs a simulation study for a given set of scenarios, analyzing simulated data using the fixed effect model, pooled and separate analyses, and timemachine and MAP prior approach. Performs inference for all treatment arms in the trial except for the first one
 #'
@@ -7,6 +7,10 @@
 #' @param arms Vector with treatment arms to perform inference on. Default - all arms except the first one
 #' @param models Vector with models that should be used for the analysis. Default=c("fixmodel", "sepmodel", "poolmodel", "timemachine", "MAPprior")
 #' @param endpoint Endpoint indicator. "cont" for continuous endpoints, "bin" for binary endpoints
+#'
+#' @importFrom parallel detectCores
+#' @importFrom doParallel registerDoParallel
+#' @importFrom foreach foreach
 #'
 #' @keywords internal
 #'
@@ -17,7 +21,10 @@
 #' @author Pavla Krotka
 
 
-sim_study <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", "poolmodel", "timemachine", "mixmodel", "MAPprior"), endpoint){
+sim_study_par <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", "poolmodel", "timemachine", "mixmodel", "MAPprior"), endpoint){
+
+  cores <- detectCores()-1 # not to overload your computer
+  registerDoParallel(cores)
 
   if (endpoint=="cont") {
     models <- models[models!="MAPprior"] # not implemented yet
@@ -47,7 +54,25 @@ sim_study <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", 
       theta_i <- as.numeric(scenarios[i, grepl("^theta\\d", names(scenarios))])
       lambda_i <- as.numeric(scenarios[i, grepl("^lambda\\d", names(scenarios))])
 
-      db <- replicate(nsim,
+      # db <- replicate(nsim,
+      #                 all_models(data = datasim_cont(n_arm = scenarios$n_arm[i],
+      #                                                num_arms = scenarios$num_arms[i],
+      #                                                d = d_i,
+      #                                                period_blocks = scenarios$period_blocks[i],
+      #                                                mu0 = scenarios$mu0[i],
+      #                                                theta = theta_i,
+      #                                                lambda = lambda_i,
+      #                                                sigma = scenarios$sigma[i],
+      #                                                trend = scenarios$trend[i],
+      #                                                N_peak = scenarios$N_peak[i],
+      #                                                full = FALSE),
+      #                            arms = arms,
+      #                            models = models,
+      #                            endpoint = endpoint,
+      #                            alpha = scenarios$alpha[i]))
+
+      db <- foreach(icount(nsim), .combine = cbind,
+                    .packages = c("NCC")) %dopar% {
                       all_models(data = datasim_cont(n_arm = scenarios$n_arm[i],
                                                      num_arms = scenarios$num_arms[i],
                                                      d = d_i,
@@ -62,7 +87,9 @@ sim_study <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", 
                                  arms = arms,
                                  models = models,
                                  endpoint = endpoint,
-                                 alpha = scenarios$alpha[i]))
+                                 alpha = scenarios$alpha[i])
+
+                    }
 
       result_i <- cbind(scenarios[i,],
                         study_arm = rep(arms, each = num_models),
@@ -92,7 +119,25 @@ sim_study <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", 
       OR_i <- as.numeric(scenarios[i, grepl("^OR\\d", names(scenarios))])
       lambda_i <- as.numeric(scenarios[i, grepl("^lambda\\d", names(scenarios))])
 
-      db <- replicate(nsim,
+      # db <- replicate(nsim,
+      #                 all_models(data = datasim_bin(n_arm = scenarios$n_arm[i],
+      #                                               num_arms = scenarios$num_arms[i],
+      #                                               d = d_i,
+      #                                               period_blocks = scenarios$period_blocks[i],
+      #                                               p0 = scenarios$p0[i],
+      #                                               OR = OR_i,
+      #                                               lambda = lambda_i,
+      #                                               trend = scenarios$trend[i],
+      #                                               N_peak = scenarios$N_peak[i],
+      #                                               full = FALSE),
+      #                            arms = arms,
+      #                            models = models,
+      #                            endpoint = endpoint,
+      #                            alpha = scenarios$alpha[i]))
+
+
+      db <- foreach(icount(nsim), .combine = cbind,
+                    .packages = c("NCC")) %dopar% {
                       all_models(data = datasim_bin(n_arm = scenarios$n_arm[i],
                                                     num_arms = scenarios$num_arms[i],
                                                     d = d_i,
@@ -106,7 +151,8 @@ sim_study <- function(nsim, scenarios, arms, models = c("fixmodel", "sepmodel", 
                                  arms = arms,
                                  models = models,
                                  endpoint = endpoint,
-                                 alpha = scenarios$alpha[i]))
+                                 alpha = scenarios$alpha[i])
+                    }
 
       result_i <- cbind(scenarios[i,],
                         study_arm = rep(arms, each = num_models),
