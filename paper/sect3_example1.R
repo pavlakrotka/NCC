80b1d027810d3155462a4a3e6dbf9aaac2348fb8
Pavla Krotka <pavla.krotka@gmail.com>
2023-02-07 19:21:15 +0100

    code paper update

--- paper/sect3_example1.R
+++ paper/sect3_example1.R
@@ -1,21 +1,23 @@
 ##################################################################################
 # Code for the paper
-# ``NCC: An R-package for analysis and simulation of platform trials with non-concurrent controls'' by Pavla Krotka et al. (2023)
-# Example - Sect 3
+# "NCC: An R-package for analysis and simulation of platform trials with non-concurrent controls" by Pavla Krotka et al. (2023)
+# Example - Section 4
 ##################################################################################
 
 library(NCC)
 
-set.seed(2346)
-data <- datasim_bin(num_arms = 3, n_arm = 100, d = c(0, 100, 250),
+set.seed(123)
+trial_data <- datasim_bin(num_arms = 3, n_arm = 100, d = c(0, 100, 250),
                     p0 = 0.7, OR = rep(1.8, 3), lambda = rep(0.15, 4), trend="stepwise")
 
-head(data)
-plot_trial(data$treatment)
+head(trial_data)
 
-fmodel <- fixmodel_bin(data=data,arm=3)
-summary(fmodel$model)
+plot_trial(trial_data$treatment)
+#ggplot2::ggsave("plot_trial.png", width = 7, height = 4)
 
-MAPprior_bin(data=data,arm=3)
+fixmodel_bin(data=trial_data, arm=3, alpha=0.025)
+summary(fixmodel_bin(data=trial_data, arm=3)$model)
 
-timemachine_bin(data=data,arm=3)
+MAPprior_bin(data=trial_data, arm=3, alpha=0.025)
+
+timemachine_bin(data=trial_data, arm=3, alpha=0.025)
--- paper/sect3_example2.R
+++ paper/sect3_example2.R
@@ -1,31 +1,92 @@
 ##################################################################################
 # Code for the paper
-# ``NCC: An R-package for analysis and simulation of platform trials with non-concurrent controls'' by Pavla Krotka et al. (2023)
-# Example 2 - Sect 3
+# "NCC: An R-package for analysis and simulation of platform trials with non-concurrent controls" by Pavla Krotka et al. (2023)
+# Example 2 - Section 4
 ##################################################################################
 
 library(NCC)
 library(ggplot2)
 
-lambda_values <- rep(seq(-0.15, 0.15, length.out = 9), 2)
-sim_scenarios <- data.frame(num_arms = 4, 
-                            n_arm = 250, 
+# lambda_values <- rep(seq(-0.15, 0.15, length.out = 9), 2)
+# sim_scenarios <- data.frame(num_arms = 4,
+#                             n_arm = 250,
+#                             d1 = 250*0,
+#                             d2 = 250*1,
+#                             d3 = 250*2,
+#                             d4 = 250*3,
+#                             period_blocks = 2,
+#                             mu0 = 0,
+#                             sigma = 1,
+#                             theta1 = 0,
+#                             theta2 = 0,
+#                             theta3 = 0,
+#                             theta4 = 0,
+#                             lambda0 = lambda_values,
+#                             lambda1 = lambda_values,
+#                             lambda2 = lambda_values,
+#                             lambda3 = lambda_values,
+#                             lambda4 = lambda_values,
+#                             trend = c(rep("linear", 9), rep("stepwise_2", 9)),
+#                             alpha = 0.025,
+#                             ncc = TRUE)
+#
+# head(sim_scenarios)
+#
+# set.seed(1234)
+# sim_results <- sim_study_par(nsim = 1000, scenarios = sim_scenarios, arms = 4,
+#                              models = c("fixmodel", "sepmodel", "poolmodel"), endpoint = "cont")
+#
+# head(sim_results)
+#
+# ggplot(sim_results, aes(x=lambda0, y=reject_h0, color=model)) +
+#   geom_point() +
+#   geom_line() +
+#   facet_grid(~ trend) +
+#   geom_hline(aes(yintercept = 0.025), linetype = "dotted") +
+#   labs(x="Strength of time trend", y="Type I error", color="Analysis approach") +
+#   theme_bw()
+# #ggsave("t1e.png", width = 7, height = 5)
+#
+# ggplot(sim_results, aes(x=lambda0, y=bias, color=model)) +
+#   geom_point() +
+#   geom_line() +
+#   facet_grid(~ trend) +
+#   geom_hline(aes(yintercept = 0), linetype = "dotted") +
+#   labs(x="Strength of time trend", y="Bias", color="Analysis approach") +
+#   theme_bw()
+# #ggsave("bias.png", width = 7, height = 5)
+#
+# ggplot(sim_results, aes(x=lambda0, y=MSE, color=model)) +
+#   geom_point() +
+#   geom_line() +
+#   facet_grid(~ trend) +
+#   labs(x="Strength of time trend", y="MSE", color="Analysis approach") +
+#   theme_bw()
+# #ggsave("mse.png", width = 7, height = 5)
+
+
+##################################################################################
+
+# Binary case:
+
+lambda_values <- rep(seq(-0.5, 0.5, length.out = 9), 2)
+sim_scenarios <- data.frame(num_arms = 4,
+                            n_arm = 250,
                             d1 = 250*0,
                             d2 = 250*1,
                             d3 = 250*2,
                             d4 = 250*3,
-                            period_blocks = 2, 
-                            mu0 = 0,
-                            sigma = 1,
-                            theta1 = 0,
-                            theta2 = 0,
-                            theta3 = 0,
-                            theta4 = 0,
-                            lambda0 = rep(seq(-0.15, 0.15, length.out = 9), 2),
-                            lambda1 = rep(seq(-0.15, 0.15, length.out = 9), 2),
-                            lambda2 = rep(seq(-0.15, 0.15, length.out = 9), 2),
-                            lambda3 = rep(seq(-0.15, 0.15, length.out = 9), 2),
-                            lambda4 = rep(seq(-0.15, 0.15, length.out = 9), 2),
+                            period_blocks = 2,
+                            p0 = 0.7,
+                            OR1 = 1,
+                            OR2 = 1,
+                            OR3 = 1,
+                            OR4 = 1,
+                            lambda0 = lambda_values,
+                            lambda1 = lambda_values,
+                            lambda2 = lambda_values,
+                            lambda3 = lambda_values,
+                            lambda4 = lambda_values,
                             trend = c(rep("linear", 9), rep("stepwise_2", 9)),
                             alpha = 0.025,
                             ncc = TRUE)
@@ -33,8 +94,8 @@ sim_scenarios <- data.frame(num_arms = 4,
 head(sim_scenarios)
 
 set.seed(1234)
-sim_results <- sim_study_par(nsim = 1000, scenarios = sim_scenarios, arms = 4, 
-                             models = c("fixmodel", "sepmodel", "poolmodel"), endpoint = "cont")
+sim_results <- sim_study_par(nsim = 1000, scenarios = sim_scenarios, arms = 4,
+                             models = c("fixmodel", "sepmodel", "poolmodel"), endpoint = "bin")
 
 head(sim_results)
 
@@ -45,6 +106,7 @@ ggplot(sim_results, aes(x=lambda0, y=reject_h0, color=model)) +
   geom_hline(aes(yintercept = 0.025), linetype = "dotted") +
   labs(x="Strength of time trend", y="Type I error", color="Analysis approach") +
   theme_bw()
+#ggsave("t1e.png", width = 7, height = 5)
 
 ggplot(sim_results, aes(x=lambda0, y=bias, color=model)) +
   geom_point() +
@@ -53,6 +115,7 @@ ggplot(sim_results, aes(x=lambda0, y=bias, color=model)) +
   geom_hline(aes(yintercept = 0), linetype = "dotted") +
   labs(x="Strength of time trend", y="Bias", color="Analysis approach") +
   theme_bw()
+#ggsave("bias.png", width = 7, height = 5)
 
 ggplot(sim_results, aes(x=lambda0, y=MSE, color=model)) +
   geom_point() +
@@ -60,3 +123,4 @@ ggplot(sim_results, aes(x=lambda0, y=MSE, color=model)) +
   facet_grid(~ trend) +
   labs(x="Strength of time trend", y="MSE", color="Analysis approach") +
   theme_bw()
+#ggsave("mse.png", width = 7, height = 5)
